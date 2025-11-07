3bd122ea94976d90b4cb4e16bf3a22489afb2282
sonnd <sonnd-han185@zitga.com.vn>
2025-11-08 00:11:51 +0700

    sửa lỗi

--- Assets/Scripts/Runtime/Manager/BuffManager.cs
+++ Assets/Scripts/Runtime/Manager/BuffManager.cs
@@ -11,7 +11,9 @@ namespace Runtime.Manager
 {
     public class BuffManager : MonoBehaviour
     {
-        [SerializeField] private List<BaseBuff> poolBuff = new();
+        [SerializeField] private List<BaseBuff> buffLevel1 = new();
+        [SerializeField] private List<BaseBuff> buffLevel2 = new();
+        [SerializeField] private List<BaseBuff> otherBuffs = new();
         
         private List<BaseBuff> _buffs = new();
 
@@ -25,6 +27,23 @@ namespace Runtime.Manager
         public void ShowSelectBuff(BuffRarity buffRarity)
         {
             Time.timeScale = 0;
+
+            var poolLevel2Ids = new List<int>();
+            foreach (var buff in _buffs)
+            {
+                if (buff.upgradeId > 0)
+                {
+                    poolLevel2Ids.Add(buff.upgradeId);
+                }
+            }
+            
+            var poolBuffLevel2 = buffLevel2.Where(x => poolLevel2Ids.Contains(x.upgradeId)).ToList();
+
+            var poolBuff = new List<BaseBuff>();
+            poolBuff.AddRange(buffLevel1);
+            poolBuff.AddRange(poolBuffLevel2);
+            poolBuff.AddRange(otherBuffs);
+            
             var buffShows = poolBuff.Where(x => x.rarity == buffRarity).Except(_buffs).Shuffle().Take(3).ToList();
 
             var data = new ShowBuffModalData()
@@ -56,7 +75,7 @@ namespace Runtime.Manager
             {
                 var removeBuff = _buffs[indexRemove];
                 _buffs.RemoveAt(indexRemove);
-                poolBuff.Remove(removeBuff);
+                RemoveBuff(removeBuff);
                 removeBuff.Remove(_weaponStat);
             }
             
@@ -64,6 +83,16 @@ namespace Runtime.Manager
             selectedBuff.Apply(_weaponStat);
             
             Time.timeScale = 1;
+        }
+
+        private void RemoveBuff(BaseBuff buff)
+        {
+            if (buffLevel1.Contains(buff))
+                buffLevel1.Remove(buff);
+            if (buffLevel2.Contains(buff))
+                buffLevel2.Remove(buff);
+            if (otherBuffs.Contains(buff))
+                otherBuffs.Remove(buff);
         }
     }
 }
